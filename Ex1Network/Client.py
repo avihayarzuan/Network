f948b8ef042353f8704894ad7b61e2022b180b7e
ronen <ronenuri@gmail.com>
2018-11-01 17:34:32 +0200

    client sababa

--- Ex1Network/Client.py
+++ Ex1Network/Client.py
@@ -1,11 +1,15 @@
+# coding: utf-8
+import sys
 from socket import socket, AF_INET, SOCK_DGRAM
+
 s = socket(AF_INET, SOCK_DGRAM)
-dest_ip = '127.0.0.1'
-dest_port = 8080
-msg = raw_input("Message to send: ")
-while not msg == 'quit':
-	s.sendto(msg, (dest_ip,dest_port))
-	data, sender_info = s.recvfrom(2048)
-	print "Server sent: ", data
-	msg = raw_input("Message to send: ")
+dest_ip = '127.0.0.1'  # sys.argv[1]
+dest_port = 8080  # sys.argv[2]
+print "Please enter a website address!\nTo quit - simply type quit"
+web_add = raw_input()
+while not web_add == 'quit':
+	s.sendto(web_add, (dest_ip, dest_port))
+	web_ip, sender_info = s.recvfrom(2048)
+	print web_ip
+	web_add = raw_input()
 s.close()
