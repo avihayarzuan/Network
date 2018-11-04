513dafe8d47d25a3d85291461dc8c0b68c87e657
ronenuri <ronenuri@gmail.com>
2018-11-04 18:25:34 +0200

    Added some comments, just need to wireshark this

--- Ex1Network/Client.py
+++ Ex1Network/Client.py
@@ -2,15 +2,17 @@
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 
+# creating our udp socket and saving our program arguments
 s = socket(AF_INET, SOCK_DGRAM)
 dest_ip = sys.argv[1]
 dest_port = int(sys.argv[2])
 print "Please enter a website address!\nTo quit - simply type quit..."
 web_add = raw_input()
+# sending the request to our server and printing the response
 while not web_add == 'quit':
     s.sendto(web_add, (dest_ip, dest_port))
     web_ip, sender_info = s.recvfrom(2048)
     print web_ip
     web_add = raw_input()
+# lastly closing the socket
 s.close()
-
--- Ex1Network/Server.py
+++ Ex1Network/Server.py
@@ -4,10 +4,12 @@ from socket import socket, AF_INET, SOCK_DGRAM
 
 
 def read_from_path():
+    """ Simple function for creating our dictionary"""
     path = sys.argv[4]
     d = {}
     with open(path) as f:
         for line in f:
+            # making sure to ignore empty lines
             if line.strip():
                 (key, val) = line.split(',')
                 d[key] = val
@@ -15,6 +17,7 @@ def read_from_path():
 
 
 def write_to_path(key, val):
+    """ Function for appending and updating our ips.txt"""
     path = sys.argv[4]
     line = key + "," + val
     file = open(path, "a")
@@ -23,6 +26,8 @@ def write_to_path(key, val):
 
 
 def ask_daddy(website_key):
+    """ Function for addressing father server,
+        after no match was found in current dictionary"""
     parent_ip = sys.argv[2]
     parent_port = int(sys.argv[3])
     msg = website_key
@@ -33,6 +38,7 @@ def ask_daddy(website_key):
 
 
 def find_address(dic, website_key):
+    """ Return a value in dictionary if found or ask father server"""
     if dic.has_key(website_key):
         return dic.get(website_key)
     else:
@@ -41,15 +47,18 @@ def find_address(dic, website_key):
         return answer
 
 
+# first loading our dictionary to the server
 dic = read_from_path()
 s = socket(AF_INET, SOCK_DGRAM)
 my_port = int(sys.argv[1])
 print "Server is running on port " + str(my_port) + "\n"
 source_ip = sys.argv[2]
+# noticing whether the server is the almighty last father or not
 if source_ip != str(-1):
     s.bind((source_ip, my_port))
 else:
     s.bind(('', my_port))
+# receiving and answering client requests indefinably
 while True:
     websiteKey, sender_info = s.recvfrom(2048)
     s.sendto(find_address(dic, websiteKey), sender_info)
